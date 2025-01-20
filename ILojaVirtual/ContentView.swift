4fa434f2a3c98fe1a771f942b960f1301ecf5642
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2025-01-20 15:09:31 -0400

    Initial Commit

---
+++ ILojaVirtual/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ILojaVirtual
+//
+//  Created by Silvanei  Martins on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ILojaVirtual/ILojaVirtualApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILojaVirtualApp.swift
+//  ILojaVirtual
+//
+//  Created by Silvanei  Martins on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ILojaVirtualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
